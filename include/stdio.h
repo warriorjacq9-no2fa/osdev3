61c3b441734bdb8793488b7cde9ef9ed323f7f9f
warriorjacq9-no2fa <jflusche3180@student.pps.net>
2026-03-11 15:35:36 +0000

    Add sprintf to stdio.h

--- include/stdio.h
+++ include/stdio.h
@@ -8,5 +8,7 @@ void putc(char c);
 
 int printf(const char* format, ...);
 int vprintf(const char* format, va_list list);
+int sprintf(char* str, const char* format, ...);
+int vsprintf(char* str, const char* format, va_list list);
 
 #endif
