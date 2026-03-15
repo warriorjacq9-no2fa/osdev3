c43b30748722665d38d9d1fe818d6d8a056b505d
warriorjacq9-no2fa <jflusche3180@student.pps.net>
2026-03-15 16:17:08 -0700

    ps/2: Add shift and caps

--- drivers/ps2.c
+++ drivers/ps2.c
@@ -19,27 +19,67 @@ static char ps2_sc1[] = {
     0, 0, 0, 0
 };
 
-static bool ext = false;
+static char upper[] = {
+    '"', '(', ')', '*', '+', '<', '_', '>', '?',
+    ')', '!', '@', '#', '$', '%', '^', '&', '*', '(',
+    ':', ':', '<', '+'
+};
+
+static bool ext, shift, caps;
 
 void ps2_irq() {
-    uint8_t keycode = inb(0x60);
-    if(keycode == 0xE0) {
-        ext = 1;
-    } else if(ext) {
-        ext = 0;
-    } else if(keycode < KEY_F12 || keycode & 0x80) {
-        kevent_input_t evt;
-        evt.type = KEVENT_KEY;
+    uint8_t sc = inb(0x60);
+    kevent_input_t evt;
+    evt.type = KEVENT_KEY;
+    if(ext); // TODO: PS/2 extended
+    else {
         // No translation needed for non-extended SC1 keys since
         // they line up with the Kernel API keycodes
-        evt.key.keycode = keycode;
-        evt.key.pressed = (keycode & 0x80) ? false : true;
+        evt.key.keycode = sc;
+        evt.key.pressed = (sc & 0x80) ? false : true;
         kinput(&evt);
-
+        
         // Send a character event as well
-        if(keycode < sizeof(ps2_sc1) && ps2_sc1[keycode]) {
-            evt.type = KEVENT_CHAR;
-            evt.ch.character = ps2_sc1[keycode];
+        evt.type = KEVENT_CHAR;
+        char c = 0;
+        switch(sc) {
+            case 0xE0: ext = true; break;
+
+            case 0x2A: // LSHIFT
+            case 0x36: // RSHIFT
+                shift = true;
+                break;
+
+            case 0xAA: // LSHIFT
+            case 0xB6: // RSHIFT
+                shift = false;
+                break;
+            
+            case 0x3A: // CAPSLK
+                caps = !caps;
+                break;
+            
+            default:
+                if(sc < sizeof(ps2_sc1) && ps2_sc1[sc]) {
+                    c = ps2_sc1[sc];
+                    if(shift) {
+                        switch(c) {
+                            case '[': c = '{'; break;
+                            case '\\': c = '|'; break;
+                            case ']': c = '}'; break;
+                            case '`': c = '~'; break;
+
+                            default:
+                                if(c - '\'' < sizeof(upper)) c = upper[c - '\''];
+                                break;
+                        }
+                    }
+                    if(shift != caps && isalpha(c)) c -= ('a' - 'A');
+                }
+                break;
+        }
+        if(c) {
+            evt.ch.character = c;
             kinput(&evt);
         }
     }
