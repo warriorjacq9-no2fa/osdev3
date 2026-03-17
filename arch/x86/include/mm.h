8aec8a68995673d26d3963df05088366542d61d5
warriorjacq9-no2fa <jflusche3180@student.pps.net>
2026-03-17 18:52:29 +0000

    Add page_alloc

--- arch/x86/include/mm.h
+++ arch/x86/include/mm.h
@@ -3,6 +3,10 @@
 
 #include <stdint.h>
 
+#define MEM_RW          0x02
+#define MEM_USER        0x04
+#define MEM_CACHELESS   0x10
+
 void mm_init();
 
 #endif
--- arch/x86/mm.c
+++ arch/x86/mm.c
@@ -3,23 +3,24 @@
 #include <string.h>
 
 #define P_PRESENT   0x01
-#define P_RW        0x02
-#define P_USER      0x04
-#define P_WTHROUGH  0x08
-#define P_NOCACHE   0x10
 #define P_ACCESSED  0x20
 #define P_DIRTY     0x40
 #define P_1MB       0x80
 #define P_GLOBAL    0x100
 
-#define PHYS(addr) ((uint32_t)(addr))
+#define PAGE_SIZE   0x1000
+#define MAX_PAGES   (2^32 / PAGE_SIZE)
+
+#define PHYS(addr) ((uint32_t)addr - 0xC0000000)
 
 #define i_pagedir(n) ((n >> 22) & 0x3FF)
 #define i_pagetab(n) ((n >> 12) & 0x3FF)
 
 static uint32_t pagedir[1024] __attribute__((aligned(4096)));
 static uint32_t pagetab_id[1024] __attribute__((aligned(4096)));
-static uint32_t pagetab_hh[1024] __attribute__((aligned(4096)));
+
+// The main bitmap for all memory
+static uint32_t bitmap[MAX_PAGES / 32];
 
 void mm_init() {
     memset(pagedir, 0, sizeof(pagedir));
@@ -27,12 +28,22 @@ void mm_init() {
     // Identity map the lower pagetable
     // and map the higher half
     for(int i = 0; i < 1024; i++) {
-        pagetab_id[i] = (i * 0x1000) | P_PRESENT | P_RW;
-        pagetab_hh[i] = (i * 0x1000) | P_PRESENT | P_RW;
+        pagetab_id[i] = (i * PAGE_SIZE) | P_PRESENT | MEM_RW;
     }
-    pagedir[0] = PHYS(pagetab_id) | P_PRESENT | P_RW;
-    pagedir[i_pagedir(0xC0000000)] = PHYS(pagetab_hh) | P_PRESENT | P_RW;
+    pagedir[0] = ((uint32_t)pagetab_id) | P_PRESENT | MEM_RW;
+    pagedir[i_pagedir(0xC0000000)] = ((uint32_t)pagetab_id) | P_PRESENT | MEM_RW;
 
-    set_cr3(PHYS(pagedir));
+    set_cr3((uint32_t)pagedir);
     set_cr0(get_cr0() | 0x80000000);
+}
+
+void* alloc_page() {
+    for(int i = 0; i < sizeof(bitmap); i++) {
+        if(~bitmap[i]) {
+            int bit = __builtin_ctz(~bitmap[i]);
+            bitmap[i] |= (1 << bit);
+            return (void*)((i * 32 + bit) * PAGE_SIZE);
+        }
+    }
+    return 0;
 }
