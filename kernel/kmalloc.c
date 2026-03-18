ee0266856bee2e33f6c845bd67cea83838dc344c
warriorjacq9-no2fa <jflusche3180@student.pps.net>
2026-03-18 22:33:34 +0000

    WIP: kfree

--- kernel/kmalloc.c
+++ kernel/kmalloc.c
@@ -3,23 +3,56 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef struct kheap_block {
+typedef struct kheap_header {
     size_t size;
     bool free;
-    struct kheap_block* next;
-} kheap_block_t;
+} kheap_header_t;
 
-extern uint32_t __kheap_start;
+typedef struct kheap_footer {
+    kheap_header_t* header;
+} kheap_footer_t;
+
+extern size_t __kheap_start;
 
 static void* heap_ptr;
 
+static size_t has_free;
+
 void kheap_init() {
-    heap_ptr = (void*)(((uint32_t)&__kheap_start + 0xFFF) & 0xFFFFF000);
-    kprintf(LOG_INFO, "kmalloc", "Heap at %08X (%08X)\r\n", (uint32_t)heap_ptr, &__kheap_start);
+    heap_ptr = (void*)(((size_t)&__kheap_start + 0xFFF) & 0xFFFFF000);
+    has_free = 0;
+    kprintf(LOG_INFO, "kmalloc", "Heap at %p (%08X)\r\n", heap_ptr, &__kheap_start);
 }
 
 void* kmalloc(size_t size) {
-    void* addr = heap_ptr;
-    heap_ptr += size;
-    return addr;
+    size_t a_size = (size + 0xF) & ~0xF;
+    if(has_free) {
+        kheap_footer_t *ftr = (kheap_footer_t*)((char*)heap_ptr - sizeof(kheap_footer_t));
+        kheap_header_t *hdr = (kheap_header_t*)ftr->header;
+        while(hdr && !hdr->free) {
+            ftr = (kheap_footer_t*)((char*)hdr - sizeof(kheap_footer_t));
+            hdr = (kheap_header_t*)ftr->header;
+        }
+        has_free--;
+        return (void*)((char*)hdr + sizeof(kheap_header_t));
+    } else {
+        kheap_header_t *hdr = (kheap_header_t*)heap_ptr;
+        hdr->free = false;
+        hdr->size = a_size;
+        heap_ptr += sizeof(kheap_header_t);
+
+        void* addr = heap_ptr;
+        heap_ptr += a_size;
+
+        kheap_footer_t *ftr = (kheap_footer_t*)heap_ptr;
+        ftr->header = hdr;
+        heap_ptr += sizeof(kheap_footer_t);
+        return addr;
+    }
+}
+
+void kfree(void* addr) {
+    kheap_header_t *hdr = (kheap_header_t*)((uint32_t)addr - sizeof(kheap_header_t));
+    hdr->free = true;
+    has_free++;
 }
