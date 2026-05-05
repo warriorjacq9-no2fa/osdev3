1b2e59bb58b97da558b00f820f67c00a5a0ea890
warriorjacq9-no2fa <jflusche3180@student.pps.net>
2026-05-05 21:15:45 +0000

    Rough VFS interface

--- fs/ext2.c
+++ fs/ext2.c
@@ -114,8 +114,7 @@ void ext2_print_tree(ext2_inode_t* inode, int d) {
         return;
     }
     ext2_dir_entry_t* dir = (ext2_dir_entry_t*)inode_get_data(inode);
-    if(dir == NULL) return;
-    while(dir->inode) {
+    while(dir && dir->inode) {
         if(dir->rec_len < 8 || dir->rec_len % 4 != 0)
             break;
         if(dir->name[0] != '.') {
@@ -136,40 +135,6 @@ void ext2_print_tree(ext2_inode_t* inode, int d) {
         dir = (ext2_dir_entry_t*)((uint8_t*)dir + dir->rec_len);
     }
     kfree(dir);
-}
-
-int ext2_init(bdev_read_t _read, size_t _vol_start) {
-    vol_start = _vol_start;
-    read = _read;
-    sb = kmalloc(sizeof(ext2_sb_t), 0);
-    if(read((void*)sb, vol_start + 1024, sizeof(ext2_sb_t))) return 1;
-    if(sb->magic != EXT2_SUPER_MAGIC) {
-        kprintf(LOG_ERR, "ext2", "Invalid magic\r\n");
-        return 1;
-    }
-    if(sb->rev_level >= 1) {
-        kprintf(LOG_INFO, "ext2", "Extended superblock is present\r\n");
-        ext_sb = kmalloc(sizeof(ext2_sb_ext_t), 0);
-        if(read((void*)ext_sb, vol_start + 1024 + sizeof(ext2_sb_t), sizeof(ext2_sb_ext_t)))
-            return 1;
-    }
-    kprintf(
-        LOG_INFO, "ext2", "%u blocks total (%u remaining), size is %u\r\n",
-        sb->blocks_count, sb->free_blocks_count, 1024 << sb->log_block_size
-    );
-    kprintf(
-        LOG_INFO, "ext2", "%u blocks per group, %u inodes per group, starting block at %u\r\n",
-        sb->blocks_per_group, sb->inodes_per_group, sb->first_data_block
-    );
-
-    size_t bgdt_block = (sb->log_block_size == 0) ? 2 : 1;
-    size_t bgdt_size = sizeof(ext2_bgdesc_t) * ((sb->blocks_count + sb->blocks_per_group) / sb->blocks_per_group);
-    
-    bgdesc_table = kmalloc(bgdt_size, 0);
-    if(read((void*)bgdesc_table, block_offset(bgdt_block), bgdt_size))
-        return 1;
-    ext2_print_tree(get_fp("arch"), 0);
-    return 0;
 }
 
 ext2_inode_t* lookup_inode(ext2_inode_t* i_dir, char* name) {
@@ -209,4 +174,39 @@ ext2_inode_t* get_fp(char* filepath) {
 
     kfree(path);
     return i;
+}
+
+int ext2_init(bdev_read_t _read, size_t _vol_start) {
+    vol_start = _vol_start;
+    read = _read;
+    sb = kmalloc(sizeof(ext2_sb_t), 0);
+    if(read((void*)sb, vol_start + 1024, sizeof(ext2_sb_t))) return 1;
+    if(sb->magic != EXT2_SUPER_MAGIC) {
+        kprintf(LOG_ERR, "ext2", "Invalid magic\r\n");
+        return 1;
+    }
+    if(sb->rev_level >= 1) {
+        kprintf(LOG_INFO, "ext2", "Extended superblock is present\r\n");
+        ext_sb = kmalloc(sizeof(ext2_sb_ext_t), 0);
+        if(read((void*)ext_sb, vol_start + 1024 + sizeof(ext2_sb_t), sizeof(ext2_sb_ext_t)))
+            return 1;
+    }
+    kprintf(
+        LOG_INFO, "ext2", "%u blocks total (%u remaining), size is %u\r\n",
+        sb->blocks_count, sb->free_blocks_count, 1024 << sb->log_block_size
+    );
+    kprintf(
+        LOG_INFO, "ext2", "%u blocks per group, %u inodes per group, starting block at %u\r\n",
+        sb->blocks_per_group, sb->inodes_per_group, sb->first_data_block
+    );
+
+    size_t bgdt_block = (sb->log_block_size == 0) ? 2 : 1;
+    size_t bgdt_size = sizeof(ext2_bgdesc_t) * ((sb->blocks_count + sb->blocks_per_group) / sb->blocks_per_group);
+    
+    bgdesc_table = kmalloc(bgdt_size, 0);
+    if(read((void*)bgdesc_table, block_offset(bgdt_block), bgdt_size))
+        return 1;
+    ext2_print_tree(get_inode(EXT2_ROOT_INO), 0);
+    //ext2_print_tree(get_fp("arch"), 0);
+    return 0;
 }
--- include/fs/vfs.h
+++ include/fs/vfs.h
@@ -2,8 +2,29 @@
 #define VFS_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 // int bdev_read_t(void* buf, size_t seek, size_t len)
 typedef int (*bdev_read_t)(void*, size_t, size_t);
+
+typedef struct vfs_file file_t;
+
+// Collection of driver-implemented functions
+typedef struct vfs_driver {
+    // int open(char* filename, file_t* file)
+    int (*open)(char*, file_t*);
+    // int close(file_t* file)
+    int (*close)(file_t*);
+    // int read(file_t* file, void* buf, size_t off, size_t len)
+    int (*read)(file_t*, void*, size_t, size_t);
+    // int write(file_t* file, void* buf, size_t off, size_t len)
+    int (*write)(file_t*, void*, size_t, size_t);
+} driver_t;
+
+typedef struct vfs_file {
+    driver_t *driver;
+    uintptr_t driver_fp;
+    uint8_t state;
+} file_t;
 
 #endif
