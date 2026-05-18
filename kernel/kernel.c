5705b94bf8eafdbe91f830c6e6e9bb6ccb26d7ce
warriorjacq9-no2fa <jflusche3180@student.pps.net>
2026-05-18 15:51:36 +0000

    Remove ext2 test code

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -32,40 +32,8 @@ void kmain() {
 
     if(res < 0) {
         kprintf(LOG_WARN, "kernel", "ext2_init returned error");
-        goto ret;
     }
 
-    vnode_t fd;
-    if((res = ext2_open(&fd, "/arch/x86/include/ctx.h", O_RDONLY)) < 0) {
-        kprintf(LOG_WARN, "kernel", "ext2_open failed with code %d\r\n", res);
-        goto ret;
-    }
-
-    stat_t stats;
-    if((res = fd.ops->fstat(&fd, &stats))) {
-        kprintf(LOG_WARN, "kernel", "ext2_fstat failed with code %d\r\n", res);
-        goto ret;
-    }
-
-    kprintf(LOG_INFO, "kernel", "Stats: inode %u, mode %04o, links %u, uid:gid %04u:%04u\r\n",
-        stats.st_ino, stats.st_mode & 0x1FF, stats.st_nlink, stats.st_uid, stats.st_gid
-    );
-    kprintf(LOG_INFO, "kernel", "Stats: size %u, block size %u, blocks %u\r\n",
-        stats.st_size, stats.st_blksize, stats.st_blocks
-    );
-    kprintf(LOG_INFO, "kernel", "Stats: atime %u, mtime %u, ctime %u\r\n",
-        stats.st_atime, stats.st_mtime, stats.st_ctime
-    );
-    
-    void* buf = kmalloc(64, 0);
-    if((res = fd.ops->read(&fd, buf, 0, 64)) < 0) {
-        kprintf(LOG_WARN, "kernel", "ext2_read failed with code %d\r\n", res);
-        goto ret;
-    }
-
-    kprintf(LOG_INFO, "kernel", "Read %.*s\r\n", 64, (char*)buf);
-
-ret:
     kshell_init(256, 16);
     while(1) wait();
 }
